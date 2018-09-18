5635e3fcfa16dc8d231d6fba01239662e9f3c3f2
GURKAN-MSI <yasingurkann@gmail.com>
2018-09-18 22:12:36 +0300

    #2 #3 Complited

--- app/src/main/java/com/example/androidtools/karma/Fragment/YasinFragment1.java
+++ app/src/main/java/com/example/androidtools/karma/Fragment/YasinFragment1.java
@@ -1,8 +1,12 @@
 package com.example.androidtools.karma.Fragment;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,11 +14,99 @@ import android.support.v4.app.Fragment;
 
 import com.example.androidtools.karma.R;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class YasinFragment1 extends Fragment {
+public class YasinFragment1 extends Fragment
+{
+
+    MapView mMapView;
+    private GoogleMap googleMap;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_yasin1, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.fragment_yasin1, container, false);
+
+        mMapView = rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try
+        {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback()
+        {
+            @Override
+            public void onMapReady(GoogleMap mMap)
+            {
+                googleMap = mMap;
+
+
+                // Add a marker in Sydney and move the camera
+                Intent tmp = getActivity().getIntent();
+                double lat = tmp.getDoubleExtra("lat", 39.925);
+                double lon = tmp.getDoubleExtra("lon", 32.866);
+                LatLng phoneLocation = new LatLng(lat, lon);
+                googleMap.addMarker(new MarkerOptions().position(phoneLocation).title("Selam Buradayım.."));
+
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(phoneLocation).zoom(5).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                // For showing a move to my location button
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                googleMap.setMyLocationEnabled(true);
+            }
+        });
+
+        return rootView;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
 }
